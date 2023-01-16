be114c2f695db3a42e6df8263658975fa7f1efcb
justinexner <92640370+justinexner@users.noreply.github.com>
2023-01-16 16:39:09 +0100

    Adds Users to Rooms

--- src/pages/rooms/[id].tsx
+++ src/pages/rooms/[id].tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import { Room, PrismaClient } from '@prisma/client';
+import { Room, User, PrismaClient } from '@prisma/client';
 import { GetServerSideProps, NextPage } from 'next';
 
 // TODO: extend functionality to show users in the specified room
@@ -8,10 +8,14 @@ import { GetServerSideProps, NextPage } from 'next';
 // supply room to component by props
 
 interface RoomProps {
-  room: Room;
+  room: Room & {users: User[]}
 }
 
 const Home: NextPage<RoomProps> = ({ room }) => {
+
+  const userCards = room.users.map(user => (
+    <span>{user.name}</span>
+  ));
 
   return (
     <>
@@ -28,7 +32,7 @@ const Home: NextPage<RoomProps> = ({ room }) => {
 
         <div className="d-flex flex-column m-2" style={{gap: "1em"}}>
           {/* TODO: put user list here */}
-          placeholder
+          {userCards}
         </div>
       </main>
     </>
@@ -38,10 +42,11 @@ const Home: NextPage<RoomProps> = ({ room }) => {
 export const getServerSideProps: GetServerSideProps<RoomProps> = async (context) => {
   const roomId = Number(context.params?.id);
   const prisma = new PrismaClient();
-  let room = await prisma.room.findFirst({where: {id: roomId}})
+  let room = await prisma.room.findFirst({where: {id: roomId}, include: {users: true}})
     .catch(() => null)
   if (!room)
     return { redirect: { destination: '/', permanent: false } }
+  console.dir(room, { depth: null });
   return { props: { room } }
 }
 
