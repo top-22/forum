ccc4b5e245ece1da3750a99c6f253eadfedbe004
justinexner <92640370+justinexner@users.noreply.github.com>
2023-03-23 19:08:02 +0100

    Die Seite für "Join A Room" wurde erstellt

---
+++ src/pages/joinRoom.tsx
@@ -0,0 +1,27 @@
+import Head from 'next/head';
+import homeStyles from '../styles/home.module.css';
+import SearchBar from '../../components/searchBar';
+
+export default function joinRoom () {
+  return (
+    <div className="bg-dark vh-100">
+      <Head>
+        <title>TUC Join Room</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="text-center">
+        <h1 className="text-primary">
+          JOIN A ROOM
+        </h1>
+        
+        <SearchBar/>
+
+        <button className={homeStyles.button}>Create Room</button>
+        <button className={homeStyles.button}>Home</button>
+
+        {/* Hier muss noch mittels <RoomPreviw/> die Vorgeschlagenen Posts hinzugefügt werden*/}
+      </div>
+    </div>
+  );
+}
