9d5900cd98fceb8095e4867b0ea67826fd9a7f38
justinexner <92640370+justinexner@users.noreply.github.com>
2023-03-26 14:39:54 +0200

    headings angepasst

--- components/roomPreview.tsx
+++ components/roomPreview.tsx
@@ -3,7 +3,7 @@ import { Room } from '@prisma/client';
 export default function RoomPreview({ rooms, title }: { rooms: Room[], title: string}) {
   return (
     <div className="p-2">
-      <p className="text-white">{title}</p>
+      <h2 className="h3 text-white">{title}</h2>
       <div className="container-fluid m-0 p-0 w-100">
         <div className="overflow-auto">
           <div className="row flex-nowrap">
@@ -11,7 +11,7 @@ export default function RoomPreview({ rooms, title }: { rooms: Room[], title: st
               <div className="col mb-2" key={room.id}>
                 <div className="card text-bg-primary h-100" style={{width: "12rem"}}>
                   <div className="card-body">
-                    <h5 className="card-title"><a className="text-white" href={`/rooms/${room.id}`}>{room.name}</a></h5>
+                    <h3 className="h5 card-title"><a className="text-white " href={`/rooms/${room.id}`}>{room.name}</a></h3>
                     <p className="card-text text-secondary">{room.dsc}</p>
                   </div>
                 </div>
--- src/pages/logIn.tsx
+++ src/pages/logIn.tsx
@@ -7,12 +7,12 @@ export default function LogIn () {
       <div className="row align-items-stretch">
         <div className="col-6 d-flex flex-column text-center justify-content-center align-items-center">
           <Image src={Logo} alt="TUC Logo" className="img-fluid w-50 mb-3" />
-          <h4 className="text-primary mb-5">Willkommen beim TUC Forum!</h4>
-          <h5 className="text-light mb-3">Du hast noch keinen Account?</h5>
+          <h1 className="h4 text-primary mb-5">Willkommen beim TUC Forum!</h1>
+          <h2 className="h5 text-light mb-3">Du hast noch keinen Account?</h2>
           <button className="btn btn-primary align-self-center" type="button">Jetzt registrieren</button>
         </div>
         <div className="col-6 d-flex flex-column justify-content-end">
-          <h4 className="text-light mb-3">Anmeldung</h4>
+          <h2 className="h4 text-light mb-3">Anmeldung</h2>
           <div className="form-group mb-3 w-50">
             <input type="email" className="form-control bg-light" placeholder="Email" />
           </div>
