520b57bbf02c32abb82e3a0827d4dc0f939366f4
Joshua Jeschek <joshua@jeschek.eu>
2023-03-28 17:47:17 +0200

    style: run prettier

--- scripts/testdata.js
+++ scripts/testdata.js
@@ -1,26 +1,25 @@
-const { PrismaClient, dmmf } = require('@prisma/client');
+const { PrismaClient, dmmf } = require("@prisma/client");
 const prisma = new PrismaClient();
-const data = require('./testdata.json');
+const data = require("./testdata.json");
 
 async function main() {
-
   // delete old data
-  console.log('deleting users...')
+  console.log("deleting users...");
   await prisma.roomUser.deleteMany();
   await prisma.user.deleteMany();
-  console.log('deleting threads...')
+  console.log("deleting threads...");
   await prisma.thread.deleteMany();
-  console.log('deleting rooms...')
+  console.log("deleting rooms...");
   await prisma.roomAffiliation.deleteMany();
   await prisma.room.deleteMany();
-  console.log('deleting affiliations...')
+  console.log("deleting affiliations...");
   await prisma.affiliation.deleteMany();
 
-  console.log('===========')
+  console.log("===========");
 
-  console.log('CREATING AFFILIATIONS')
+  console.log("CREATING AFFILIATIONS");
   for (const affiliationData of data.affiliations) {
-    console.log('   ' + affiliationData.name)
+    console.log("   " + affiliationData.name);
     await prisma.affiliation.create({
       data: {
         name: affiliationData.name,
@@ -29,17 +28,31 @@ async function main() {
     });
   }
 
-  console.log('CREATING ROOMS')
+  console.log("CREATING ROOMS");
   for (const roomData of data.rooms) {
-    console.log('   ' + roomData.name)
+    console.log("   " + roomData.name);
     const roomAffiliations = [];
     for (const affiliation of roomData.affiliations.optional) {
-      const affiliationId = (await prisma.affiliation.findFirstOrThrow({where: {name: affiliation }})).id;
-      roomAffiliations.push({ type: 'OPTIONAL', affiliation: { connect: { id: affiliationId }}});
+      const affiliationId = (
+        await prisma.affiliation.findFirstOrThrow({
+          where: { name: affiliation },
+        })
+      ).id;
+      roomAffiliations.push({
+        type: "OPTIONAL",
+        affiliation: { connect: { id: affiliationId } },
+      });
     }
     for (const affiliation of roomData.affiliations.required) {
-      const affiliationId = (await prisma.affiliation.findFirstOrThrow({where: {name: affiliation }})).id;
-      roomAffiliations.push({ type: 'REQUIRED', affiliation: { connect: { id: affiliationId }}});
+      const affiliationId = (
+        await prisma.affiliation.findFirstOrThrow({
+          where: { name: affiliation },
+        })
+      ).id;
+      roomAffiliations.push({
+        type: "REQUIRED",
+        affiliation: { connect: { id: affiliationId } },
+      });
     }
     await prisma.room.create({
       data: {
@@ -52,27 +65,33 @@ async function main() {
     });
   }
 
-  console.log('CREATING THREADS')
+  console.log("CREATING THREADS");
   for (const threadData of data.threads) {
-    console.log('   ' + threadData.name)
+    console.log("   " + threadData.name);
     await prisma.thread.create({
       data: {
         name: threadData.name,
         room: {
           connect: {
-            id: (await prisma.room.findFirstOrThrow({where: {name: threadData.room}})).id,
+            id: (
+              await prisma.room.findFirstOrThrow({
+                where: { name: threadData.room },
+              })
+            ).id,
           },
         },
       },
     });
   }
 
-  console.log('CREATING USERS')
+  console.log("CREATING USERS");
   for (const userData of data.users) {
-    console.log('   ' + userData.name)
+    console.log("   " + userData.name);
     const roomUsers = [];
     for (const room of userData.rooms) {
-      const roomId = (await prisma.room.findFirstOrThrow({where: {name: room.name}})).id;
+      const roomId = (
+        await prisma.room.findFirstOrThrow({ where: { name: room.name } })
+      ).id;
       roomUsers.push({ role: room.role, room: { connect: { id: roomId } } });
     }
     await prisma.user.create({
