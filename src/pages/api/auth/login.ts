9b591e50293b8d978ce9410db5aa854339b31350
philipkbh <dev@knoblochphilip.com>
2023-04-05 15:54:14 +0200

    feat: add login (with problems)

---
+++ src/pages/api/auth/login.ts
@@ -0,0 +1,40 @@
+// pages/api/auth/login.ts
+import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import jwt, { Secret } from "jsonwebtoken";
+
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Method not allowed" });
+  }
+
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  const user = await prisma.user.findUnique({ where: { email } });
+
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+    return res.status(401).json({ message: "Invalid email or password" });
+  }
+
+  const jwtSecret = process.env.JWT_SECRET;
+
+  if (!jwtSecret) {
+    return res.status(500).json({ message: "JWT secret not configured" });
+  }
+
+  const token = jwt.sign({ id: user.id, email: user.email }, jwtSecret as Secret, {
+    expiresIn: "1d",
+  });
+
+  res.status(200).json({ message: "Logged in successfully", token });
+}
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,48 +1,82 @@
 import { NextPage } from "next";
 import Image from "next/image";
 import Logo from "../public/TUC-farbig.png";
+import { useState } from "react";
+import { useRouter } from "next/router";
 
-const Login: NextPage = () => {
+const LoginPage: NextPage = () => {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const response = await fetch("/api/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      localStorage.setItem("authToken", data.token);
+      router.push("/");
+    } else {
+      // Handle error (e.g., show an error message)
+    }
+  };
+
   return (
     <div className="bg-dark vh-100 overflow-hidden d-flex align-items-center">
       <div className="row align-items-stretch">
         <div className="col-6 d-flex flex-column text-center justify-content-center align-items-center">
           <Image src={Logo} alt="TUC Logo" className="img-fluid w-50 mb-3" />
-          <h4 className="text-primary mb-5">Willkommen beim TUC Forum!</h4>
-          <h5 className="text-light mb-3">Du hast noch keinen Account?</h5>
+          <h1 className="text-primary mb-5">Willkommen beim TUC Forum!</h1>
+          <h2 className="text-light mb-3">Du hast noch keinen Account?</h2>
           <button className="btn btn-primary align-self-center" type="button">
             Jetzt registrieren
           </button>
         </div>
         <div className="col-6 d-flex flex-column justify-content-end">
-          <h4 className="text-light mb-3">Anmeldung</h4>
-          <div className="form-group mb-3 w-50">
-            <input
-              type="email"
-              className="form-control bg-light"
-              placeholder="Email"
-            />
-          </div>
-          <div className="form-group mb-3 w-50">
-            <input
-              type="password"
-              className="form-control bg-light"
-              placeholder="Passwort"
-            />
-            <a
+          <h1 className="text-light mb-3">Anmeldung</h1>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                className="form-control bg-light"
+                id="email"
+                value={email}
+                placeholder="Email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="password">Passwort</label>
+              <input
+                type="password"
+                className="form-control bg-light"
+                id="password"
+                value={password}
+                placeholder="Passwort"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <button className="btn btn-primary align-self-start" type="submit">Login</button>
+          </form>
+          <a
               href=""
               className="text-light d-block mb-3 d-flex justify-content-end"
             >
               Passwort vergessen?
             </a>
-          </div>
-          <button className="btn btn-primary align-self-start" type="button">
-            Jetzt anmelden
-          </button>
         </div>
       </div>
     </div>
   );
 };
 
-export default Login;
+export default LoginPage;
