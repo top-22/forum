c64f631eb137d4a4600324e942830156b6adb7ef
philipkbh <dev@knoblochphilip.com>
2023-04-05 17:21:33 +0200

    style: format >:|

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -40,11 +40,9 @@ export default async function handler(
     }
   );
 
-  res
-    .status(200)
-    .json({
-      message: "Logged in successfully",
-      username: user.username,
-      token,
-    });
+  res.status(200).json({
+    message: "Logged in successfully",
+    username: user.username,
+    token,
+  });
 }
