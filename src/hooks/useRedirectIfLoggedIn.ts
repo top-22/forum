d87139b1d18e8ec126f95033b2fe0ba86ebcc1bd
philipkbh <dev@knoblochphilip.com>
2023-04-06 18:21:52 +0200

    feat: add proper redirection after successful login
    This commit adds correct redirection behavior to the login page. After a successful login, users will be redirected to the page they initially tried to access or the root page if no next parameter is available. Additionally, it prevents logged-in users from accessing the login and register pages by redirecting them to the main page.

--- src/hooks/useRedirectIfLoggedIn.ts
+++ src/hooks/useRedirectIfLoggedIn.ts
@@ -1,15 +1,15 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 
-const useAuth = () => {
+const useRedirectIfLoggedIn = () => {
   const router = useRouter();
 
   useEffect(() => {
     const authToken = localStorage.getItem("authToken");
-    if (!authToken) {
-      router.replace(`/login?next=${encodeURIComponent(router.asPath)}`);
+    if (authToken) {
+      router.replace("/");
     }
   }, [router]);
 };
 
-export default useAuth;
+export default useRedirectIfLoggedIn;
