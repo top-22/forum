3bf5361c3b9f0c02f010d4024058ab121519a225
philipkbh <dev@knoblochphilip.com>
2023-04-06 02:19:08 +0200

    style: format

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -26,18 +26,6 @@ const LoginPage: NextPage = () => {
 
     setError("");
 
-    if (!email) {
-      setError("Please fill out the email field.");
-      return;
-    }
-
-    if (!password) {
-      setError("Please fill out the password field.");
-      return;
-    }
-
-    setError("");
-
     const response = await fetch("/api/auth/login", {
       method: "POST",
       headers: {
