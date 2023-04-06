babc2250d05de6b31e3cb88cbe490d275048bb3c
philipkbh <dev@knoblochphilip.com>
2023-04-06 02:13:02 +0200

    feat: protect protected pages & redirect after login

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,15 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+const useAuth = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const authToken = localStorage.getItem('authToken');
+    if (!authToken) {
+      router.replace(`/login?next=${router.pathname}`);
+    }
+  }, [router]);
+};
+
+export default useAuth;
