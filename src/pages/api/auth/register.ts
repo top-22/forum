20d5c563ef5263c049df03008b11478fecd4d234
philipkbh <dev@knoblochphilip.com>
2023-04-06 02:39:29 +0200

    style: format

--- src/pages/api/auth/register.ts
+++ src/pages/api/auth/register.ts
@@ -13,42 +13,42 @@ export default async function handler(
     if (req.method !== "POST") {
       return res.status(405).json({ message: "Method not allowed" });
     }
-  
+
     const { name, username, email, password /*, repeatPassword*/ } = req.body;
-  
+
     // ? Do we need to check for empty fields
     // ? Client-side validation should be enough
     // if (!name || !username || !email || !password || !repeatPassword) {
     //  return res.status(400).json({ message: "All fields are required" });
     // }
-  
+
     // if (password !== repeatPassword) {
     //   return res.status(400).json({ message: "Passwords do not match" });
     // }
-  
+
     const existingUserByUsername = await prisma.user.findFirst({
       where: {
         username: username,
       },
     });
-  
+
     const existingUserByEmail = await prisma.user.findFirst({
       where: {
         email: email,
       },
     });
-  
+
     if (existingUserByUsername || existingUserByEmail) {
       return res
         .status(400)
         .json({ message: "User (username and/or email) already exists" });
     }
-  
+
     const hashedPassword = await bcrypt.hash(
       password,
       Number(process.env.SALT_ROUNDS)
     );
-  
+
     const newUser = await prisma.user.create({
       data: {
         name: name,
@@ -57,13 +57,13 @@ export default async function handler(
         password: hashedPassword,
       },
     });
-  
+
     const jwtSecret = process.env.JWT_SECRET;
-  
+
     if (!jwtSecret) {
       return res.status(500).json({ message: "JWT secret not configured" });
     }
-  
+
     const token = jwt.sign(
       { id: newUser.id, email: newUser.email },
       jwtSecret as Secret,
@@ -71,7 +71,7 @@ export default async function handler(
         expiresIn: "1d",
       }
     );
-  
+
     res.status(200).json({
       message: "User created successfully",
       username: newUser.username,
@@ -80,5 +80,4 @@ export default async function handler(
   } catch (error) {
     res.status(500).json({ message: "Internal server error", error });
   }
-  
 }
