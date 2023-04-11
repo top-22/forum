9506646a7a0adf5b660e7b5891a40d2038cf35f1
rotwolf <carl.jonathan.wolf@gmx.de>
2023-04-11 22:49:37 +0200

    feat: Threads richtig anzeigen

--- src/pages/room/[roomId].tsx
+++ src/pages/room/[roomId].tsx
@@ -1,14 +1,16 @@
 import Head from "next/head";
 import Link from "next/link";
-import { Room, PrismaClient, RoomUser } from "@prisma/client";
+import { Room, PrismaClient, RoomUser, User, Thread } from "@prisma/client";
 import { GetServerSideProps, NextPage } from "next";
 import Layout from "../../components/layout";
 
 interface RoomProps {
-  room: Room & { users: RoomUser[] };
+  room: Room;
+  users: User[];
+  threads: Thread[];
 }
 
-const Room: NextPage<RoomProps> = ({ room }) => {
+const Room: NextPage<RoomProps> = ({ room, users, threads}) => {
   return (
     <Layout>
       <Head>
@@ -20,13 +22,29 @@ const Room: NextPage<RoomProps> = ({ room }) => {
         <h1 className="text-primary">{room.name}</h1>
         <button>CREATE POST</button>
         <div className="d-flex flex-column">
-          <Link href={`${room.id}/1`}>Thread 1</Link>
-          <Link href={`${room.id}/2`}>Thread 2</Link>
-          <Link href={`${room.id}/3`}>Thread 3</Link>
-          <Link href={`${room.id}/4`}>Thread 4</Link>
-          <Link href={`${room.id}/5`}>Thread 5</Link>
+          {threads.length > 0 ? (
+            threads.map(thread => (
+              <div>
+                <Link key={thread.id} href={`${room.id}/${thread.id}`}>
+                  {`${thread.name} ${thread.description ?? "Dieser Thread hat keine genauere Beschreibung."}`} 
+                </Link>
+              </div>
+            ))
+          ) : (
+            <div>
+              Es gibt keine Threads.
+            </div>
+          )}
         </div>
         <span>{room.description}</span>
+        <div>
+          <h2>Benutzer im Raum:</h2>
+          <ul>
+            {users.map(user => (
+              <li key={user.id}>{user.name}</li>
+            ))}
+          </ul>
+        </div>
       </main>
     </Layout>
   );
@@ -38,11 +56,16 @@ export const getServerSideProps: GetServerSideProps<RoomProps> = async (
   const roomId = Number(context.params?.roomId);
   const prisma = new PrismaClient();
   let room = await prisma.room
-    .findFirst({ where: { id: roomId }, include: { users: true } })
+    .findFirst({ where: { id: roomId }})
     .catch(() => null);
   if (!room) return { redirect: { destination: "/", permanent: false } };
   console.dir(room, { depth: null });
-  return { props: { room } };
+  let users = await prisma.roomUser
+    .findMany({ where: { roomId }, include: { user: true } })
+    .then(roomUsers => roomUsers.map(roomUser => roomUser.user));
+  let threads = await prisma.thread
+    .findMany({ where: { roomId } });
+  return { props: { room, users, threads } };
 };
 
 export default Room;
