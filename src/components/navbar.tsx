2eca24fab6fce39916256e2ad7bc4f351fb6d4d9
justinexner <92640370+justinexner@users.noreply.github.com>
2023-04-12 17:57:24 +0200

    style: prettier formatiert

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,6 @@
-import { useRouter } from 'next/router';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
+import { useRouter } from "next/router";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 import Image from "next/image";
 import { FunctionComponent } from "react";
 import logo from "../public/Logo.svg";
@@ -9,24 +9,44 @@ interface NavbarProps {
   setShowSettings: (show: boolean) => void;
 }
 
-const VerticalNavbar: FunctionComponent<NavbarProps> = ({ setShowSettings }) => {
+const VerticalNavbar: FunctionComponent<NavbarProps> = ({
+  setShowSettings,
+}) => {
   const router = useRouter();
 
   return (
     <Navbar bg="primary" className="d-flex flex-column vh-100">
-      <Navbar.Brand onClick={() => router.push('/')} className="mx-auto" style={{ cursor: 'pointer' }}>
-        <Image src={logo} alt="Logo" width={50} height={50} className="rounded"/>
+      <Navbar.Brand
+        onClick={() => router.push("/")}
+        className="mx-auto"
+        style={{ cursor: "pointer" }}
+      >
+        <Image
+          src={logo}
+          alt="Logo"
+          width={50}
+          height={50}
+          className="rounded"
+        />
       </Navbar.Brand>
       <Nav className="flex-column d-flex flex-column flex-grow-1">
         {/* Add your nav items here */}
-        <Nav.Link onClick={() => router.push('/')} className="mx-auto">Home</Nav.Link>
+        <Nav.Link onClick={() => router.push("/")} className="mx-auto">
+          Home
+        </Nav.Link>
         <hr className="mx-auto w-75" /> {/* Divider after Home */}
         <div className="flex-grow-1">
-          <Nav.Link onClick={() => router.push('room/1')} className="mx-auto">Raum 1</Nav.Link>
-          <Nav.Link onClick={() => router.push('room/2')} className="mx-auto">Raum 2</Nav.Link>
+          <Nav.Link onClick={() => router.push("room/1")} className="mx-auto">
+            Raum 1
+          </Nav.Link>
+          <Nav.Link onClick={() => router.push("room/2")} className="mx-auto">
+            Raum 2
+          </Nav.Link>
         </div>
         <hr className="mx-auto w-75" /> {/* Divider before Settings */}
-        <Nav.Link onClick={() => setShowSettings(true)} className="mx-auto">Settings</Nav.Link>
+        <Nav.Link onClick={() => setShowSettings(true)} className="mx-auto">
+          Settings
+        </Nav.Link>
       </Nav>
     </Navbar>
   );
