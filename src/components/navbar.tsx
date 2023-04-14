85a53c2079a91461df02defb0933744cb6989396
justinexner <92640370+justinexner@users.noreply.github.com>
2023-04-14 17:54:45 +0200

    feat: Highlighting des aktuellen Tabs

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,6 +4,7 @@ import Nav from "react-bootstrap/Nav";
 import Image from "next/image";
 import { FunctionComponent } from "react";
 import logo from "../public/Logo.svg";
+import Link from "next/link";
 
 interface NavbarProps {
   setShowSettings: (show: boolean) => void;
@@ -14,6 +15,16 @@ const VerticalNavbar: FunctionComponent<NavbarProps> = ({
 }) => {
   const router = useRouter();
 
+  const navItems = [
+    { path: '/room/1', label: 'Page 1' },
+    { path: '/room/2', label: 'Page 2' },
+  ];
+
+  const isActive = (path: string) => {
+    return router.asPath === path;
+  };
+
+  
   return (
     <Navbar bg="primary" className="d-flex flex-column vh-100">
       <Navbar.Brand
@@ -31,21 +42,29 @@ const VerticalNavbar: FunctionComponent<NavbarProps> = ({
       </Navbar.Brand>
       <Nav className="flex-column d-flex flex-grow-1">
         {/* nav items */}
-        <Nav.Link onClick={() => router.push("/")} className="mx-auto">
+        <Nav.Link as={Link} href="/" active={isActive("/")} className="mx-auto">
           Home
         </Nav.Link>
         <hr className="mx-auto w-75" /> {/* Trenner nach Home */}
         <div className="flex-grow-1"> {/* sorgt dafür dass Settings ganz unten ist*/}
-          <Nav.Link onClick={() => router.push("room/1")} className="mx-auto">
-            Raum 1
-          </Nav.Link>
-          <Nav.Link onClick={() => router.push("room/2")} className="mx-auto">
-            Raum 2
-          </Nav.Link>
+          <div className="d-flex flex-column align-items-center"> {/* zentriert die Items */}
+            {/* Listet alle Items zwischen Home und Einstellungen */}
+            {navItems.map((item) => (
+              <Nav.Link
+                key={item.path}
+                as={Link}
+                href={item.path}
+                active={isActive(item.path)}
+                className="mx-auto"
+              >
+                {item.label}
+              </Nav.Link>
+            ))}
+          </div>
         </div>
-        <hr className="mx-auto w-75" /> {/* Trenner vor Settings */}
+        <hr className="mx-auto w-75" /> {/* Trenner vor Einstellungen */}
         <Nav.Link onClick={() => setShowSettings(true)} className="mx-auto">
-          Settings
+          Einstellungen
         </Nav.Link>
       </Nav>
     </Navbar>
