9a6bf440141a8aa62145f94ab0e5dd61848f987f
justin <92640370+justinexner@users.noreply.github.com>
2023-04-17 19:46:06 +0200

    refactor: Button bootstrap angepasst
    Co-authored-by: Joshua Jeschek <joshua@jeschek.eu>

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -26,9 +26,9 @@ const Settings = () => {
           className="img-fluid w-25 mb-3"
         />
         <h4 className="text-light mb-3 mx-auto">Nutzername</h4>
-        <Button className="btn btn-primary mb-3 w-25">Einstellungen</Button>
-        <Button className="btn btn-primary mb-3 w-25">Darkmode</Button>
-        <Button className="btn btn-primary mb-3 w-25">Abmelden</Button>
+        <Button className="mb-3 w-25">Einstellungen</Button>
+        <Button className="mb-3 w-25">Darkmode</Button>
+        <Button className="mb-3 w-25">Abmelden</Button>
       </div>
     </Layout>
   );
