a53ddccea94b47471a316734f1af22dbe7d74d3a
justinexner <92640370+justinexner@users.noreply.github.com>
2023-05-02 22:21:23 +0200

    feat: first click works -> second error

--- src/pages/room/[roomId]/[threadId].tsx
+++ src/pages/room/[roomId]/[threadId].tsx
@@ -1,27 +1,37 @@
 import Head from "next/head";
-import { Room, PrismaClient, RoomUser } from "@prisma/client";
+import { Room, RoomUser, PrismaClient, User, Thread } from "@prisma/client";
 import { GetServerSideProps, NextPage } from "next";
 import Layout from "../../../components/layout";
+import dynamic from 'next/dynamic';
+
+const RoomPage = dynamic(() => import('../[roomId]'));
 
 interface ThreadProps {
-  room: Room & { users: RoomUser[] };
+  room: Room & {
+    users: (RoomUser & { user: User })[];
+    threads: (Thread & { creator: User })[];
+  };
   thread: Number;
 }
 
 const Thread: NextPage<ThreadProps> = ({ room, thread }) => {
   return (
-    <Layout>
-      <Head>
-        <title>{`TUC Forum - ${room.name} - ${thread}`}</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1 className="text-primary">{room.name}</h1>
-        <h2 className="text-primary">{`Thread: ${thread}`}</h2>
-        <span>{room.description}</span>
-      </main>
-    </Layout>
+    <div className="row">
+      <div className="col">
+        <RoomPage room={room}></RoomPage>
+      </div>
+      <div className="col">
+        <Head>
+          <title>{`TUC Forum - ${room.name} - ${thread}`}</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <main>
+          <h1 className="text-primary">{room.name}</h1>
+          <h2 className="text-primary">{`Thread: ${thread}`}</h2>
+          <span>{room.description}</span>
+        </main>
+      </div>
+    </div>
   );
 };
 
@@ -32,7 +42,13 @@ export const getServerSideProps: GetServerSideProps<ThreadProps> = async (
   const threadId = Number(context.params?.threadId);
   const prisma = new PrismaClient();
   let room = await prisma.room
-    .findFirst({ where: { id: roomId }, include: { users: true } })
+    .findFirst({
+      where: { id: roomId },
+      include: {
+        users: { include: { user: true } },
+        threads: { include: { creator: true } },
+      },
+    })
     .catch(() => null);
   if (!room) return { redirect: { destination: "/", permanent: false } };
   console.dir(room, { depth: null });
